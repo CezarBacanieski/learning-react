b0c30601fc5648cac806a236e286459d8a430247
CezarDev <bacanieskidev@gmail.com>
2024-07-10 12:29:46 -0300

    adicioando nova lib de icon e botao de deletar em cada card

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,20 @@
-import './colaborador.css'
+import { IoIosCloseCircle } from 'react-icons/io';
+import './colaborador.css';
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className='colaborador'>
+      <IoIosCloseCircle size={25} className='deletar' onClick={aoDeletar} />
 
-export default Colaborador
+      <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className='rodape'>
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
